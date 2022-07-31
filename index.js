7cbd38cc9efb3d43a231bbdb188437a519025d8f
Galih Juansaputra <galih.juansaputra@gmail.com>
2022-07-31 11:27:06 +0800

    mencoba export import es6

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const {coffeeStock, isCoffeeMachineReady} = require('./state');
+import { coffeeStock, isCoffeeMachineReady } from './state.js';
  
-const makeCoffee = (type, miligrams) => {
-    if (coffeeStock[type] >= miligrams) {
-        console.log("Kopi berhasil dibuat!");
-    } else {
-        console.log("Biji kopi habis!");
-    }
-}
- 
-makeCoffee("robusta", 80);
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
